caa1aa848b537447314c2c9c6d994008b755e264
tuxfux.hlp <tuxfux.hlp@gmail.com>
2015-01-31 23:37:31 +0530

    my first commit

---
+++ Project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tuxfux/Documents/Projects/Training_dashboard/Project/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
